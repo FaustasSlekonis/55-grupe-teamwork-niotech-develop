76b824269bd8b66dd2805682ef58401cbd1bfaff
Faustas <faustas.slekonis@gmail.com>
2025-06-03 10:42:44 +0300

    niotech

--- js/components/header.js
+++ js/components/header.js
@@ -1,6 +1,24 @@
 export function header() {
-
-    const HTML = 'HEADER';
+    const HTML = `
+        <header class="container header">
+            <div class="row">
+                <div class="col-12 header">
+                    <img class="logo" src="./img/logo.png">
+                    <div class="header-middle">
+                    <div>Home</div>
+                    <div>About Us</div>
+                    <div>Sevices</div>
+                    <div>Pricing</div>
+                    <div>Pages</div>
+                    <div>Blogs</div>
+                    <div>Contact Us</div>
+                    </div>
+                    <i class="fa fa-search"></i>
+                    <div class="btnheader">Get Started  <i class="fa fa-long-arrow-right"></i></div>
+                    </div>
+                </div>
+            </div>
+        </header>`;
 
     document
         .getElementById('app')
