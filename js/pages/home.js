1fb91282f1c7d55c63d621ec50035ee238387550
Faustas <faustas.slekonis@gmail.com>
2025-06-06 12:12:56 +0300

    niotech

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,9 +1,15 @@
 import { header } from "../components/header.js";
-import { hero } from "../components/hero.js";
 import { features } from "../components/features.js";
 import { features2 } from "../components/features2.js";
+import { whatwedo } from "../components/whatwedo.js";
+import { testimonials } from "../components/testimonials.js";
+import { footer } from "../components/footer.js";
+
+
 
 header();
-hero();
 features();
 features2();
+whatwedo();
+testimonials();
+footer();
